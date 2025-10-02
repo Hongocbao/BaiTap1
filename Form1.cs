fcfeef284fd1ca885a45dab8c2d77bd9afcd6349
Hongocbao <ngocbao11a7locan2021@gmail.com>
2025-10-02 22:15:37 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "AVI file|*.avi|MPEG File|*.mpeg|Wav File|*.wav|Midi File|*.midi|Mp4 File|*.mp4|MP3|*.mp3";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = dlg.FileName;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.toolStripStatusLabel1.Text = string.Format("Hôm nay là ngày {0} - Bây giờ là {1}",
+                                      DateTime.Now.ToString("dd/MM/yyyy"),
+                                      DateTime.Now.ToString("hh:mm:ss tt"));
+        }
+    }
+}
